46aeedf03200f7efc2c6ee7562eedfb93e91dcef
prashantpstae-arch <prashantpstae@gmail.com>
2025-11-27 16:46:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv")
+const app = express();
+const port = 3000;
+
+dotenv.config()
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
+
+module.exports = {app}
